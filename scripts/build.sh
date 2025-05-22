2fe3f0bdbc04141244e3894535068c86ffb45516
yuantao <taoyuan15@h-partners.com>
2025-05-22 02:03:58 +0000

    !108 编译脚本安全性增强 * build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 
-SCRIPT_PATH=$(dirname $(realpath $0))
-CMAKE_SOURCE_PATH=$(realpath $SCRIPT_PATH/..)
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+CMAKE_SOURCE_PATH=$(realpath "$SCRIPT_PATH"/..)
 
 CMAKE_BUILD_PATH=$CMAKE_SOURCE_PATH/build
 
@@ -24,7 +24,7 @@ TARGET=${!#}
 echo "Target is: $TARGET"
 CMAKE_BUILD_TYPE=Release
 
-mkdir -p $CMAKE_BUILD_PATH
+mkdir -p "$CMAKE_BUILD_PATH"
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -44,41 +44,40 @@ while [[ $# -gt 0 ]]; do
 done
 
 function build_shared_lib() {
-    cd $CMAKE_SOURCE_PATH/examples/shared_lib
+    cd "$CMAKE_SOURCE_PATH"/examples/shared_lib || exit
     rm -rf build
-    cmake --no-warn-unused-cli -B build -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$OUTPUT_PATH/shared_lib -DCATLASS_INCLUDE_DIR=$CMAKE_SOURCE_PATH/include
+    cmake --no-warn-unused-cli -B build -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$OUTPUT_PATH"/shared_lib -DCATLASS_INCLUDE_DIR="$CMAKE_SOURCE_PATH"/include
     cmake --build build -j
     cmake --install build
-    cd $CMAKE_SOURCE_PATH
+    cd "$CMAKE_SOURCE_PATH" || exit
 }
 
 function build_torch_library() {
-    cd $CMAKE_SOURCE_PATH/examples/python_extension
+    cd "$CMAKE_SOURCE_PATH"/examples/python_extension || exit
     rm -rf build
-    cmake --no-warn-unused-cli -B build -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$OUTPUT_PATH/python_extension -DCATLASS_INCLUDE_DIR=$CMAKE_SOURCE_PATH/include -DPython3_EXECUTABLE=$(which python3) -DBUILD_TORCH_LIB=True
+    cmake --no-warn-unused-cli -B build -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$OUTPUT_PATH"/python_extension -DCATLASS_INCLUDE_DIR="$CMAKE_SOURCE_PATH"/include -DPython3_EXECUTABLE="$(which python3)" -DBUILD_TORCH_LIB=True
     cmake --build build -j
     cmake --install build
-    cd $CMAKE_SOURCE_PATH
+    cd "$CMAKE_SOURCE_PATH" || exit
 }
 
 function build_python_extension() {
-    cd $CMAKE_SOURCE_PATH/examples/python_extension
+    cd "$CMAKE_SOURCE_PATH"/examples/python_extension || exit
     rm -rf build
-    python3 setup.py bdist_wheel --dist-dir $OUTPUT_PATH/python_extension
-    cd $CMAKE_SOURCE_PATH
+    python3 setup.py bdist_wheel --dist-dir "$OUTPUT_PATH"/python_extension
+    cd "$CMAKE_SOURCE_PATH" || exit
 }
 
 if [[ "$TARGET" == "shared_lib" ]]; then
     build_shared_lib
 elif [[  "$TARGET" == "lib_cmake" ]]; then
-    cmake -DENABLE_LIB=ON -S $CMAKE_SOURCE_PATH -B $CMAKE_BUILD_PATH
-    cmake --build $CMAKE_BUILD_PATH
+    cmake -DENABLE_LIB=ON -S "$CMAKE_SOURCE_PATH" -B "$CMAKE_BUILD_PATH"
+    cmake --build "$CMAKE_BUILD_PATH"
 elif [[ "$TARGET" == "python_extension" ]]; then
     build_python_extension
 elif [[ "$TARGET" == "torch_library" ]]; then
     build_torch_library
 else
-    cmake --no-warn-unused-cli -S$CMAKE_SOURCE_PATH -B$CMAKE_BUILD_PATH
-    cmake --build $CMAKE_BUILD_PATH --target $TARGET -j
+    cmake --no-warn-unused-cli -S"$CMAKE_SOURCE_PATH" -B"$CMAKE_BUILD_PATH"
+    cmake --build "$CMAKE_BUILD_PATH" --target "$TARGET" -j
 fi
-
